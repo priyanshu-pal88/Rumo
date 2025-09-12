93f9ea7ec43e95b7d5bc55d5cd575a13f169e700
priyanshu-pal88 <aachipal88@gmail.com>
2025-09-13 00:28:51 +0530

    changed something

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,8 @@ io.on("connection", (socket) => {
 
 });
 
+const PORT = process.env.PORT || 3000;
 
-httpServer.listen(3000, () => {
-  console.log('Server is running on port 3000')
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
