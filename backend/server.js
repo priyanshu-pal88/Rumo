17a96afe36fcb753cd67978ed6654cb81df71e3b
priyanshu-pal88 <aachipal88@gmail.com>
2025-09-13 00:53:21 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ const generateResponse = require('./src/services/ai.service');
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.FRONTEND_URL
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    
+
   }
 });
 
